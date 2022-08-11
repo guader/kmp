63dfeeda22809ba7cce2c969fb8eaed85e2bc873
guader <nampp@live.com>
2022-08-11 15:42:25 +0800

    comments

--- kmp.go
+++ kmp.go
@@ -1,17 +1,17 @@
 package kmp
 
-func getNext[T comparable](p []T) []int {
-	l := len(p)
+func getNext[T comparable](pattern []T) []int {
+	l := len(pattern)
 	j := 0
 	k := -1
 	m := l - 1
 	next := make([]int, l, l)
 	next[0] = -1
 	for j < m {
-		if k == -1 || p[j] == p[k] {
+		if k == -1 || pattern[j] == pattern[k] {
 			j++
 			k++
-			if p[j] == p[k] {
+			if pattern[j] == pattern[k] {
 				next[j] = next[k]
 			} else {
 				next[j] = k
@@ -23,7 +23,7 @@ func getNext[T comparable](p []T) []int {
 	return next
 }
 
-func Index[T comparable](test, pattern []T) int {
+func index[T comparable](test, pattern []T, next []int) int {
 	lt := len(test)
 	lp := len(pattern)
 	if lt < lp || lp == 0 {
@@ -31,7 +31,6 @@ func Index[T comparable](test, pattern []T) int {
 	}
 	i := 0
 	j := 0
-	next := getNext(pattern)
 	for i < lt && j < lp {
 		if j == -1 || test[i] == pattern[j] {
 			i++
@@ -46,16 +45,23 @@ func Index[T comparable](test, pattern []T) int {
 	return -1
 }
 
+// Index find index of pattern from test
+func Index[T comparable](test, pattern []T) int {
+	return index(test, pattern, getNext(pattern))
+}
+
+// Indexes find indexes of patterns not overlapped from test
 func Indexes[T comparable](test, pattern []T) []int {
 	var res []int
+	next := getNext(pattern)
 	lp := len(pattern)
-	i := Index(test, pattern)
+	i := index(test, pattern, next)
 	j := 0
 	for i != -1 {
 		j += i
 		res = append(res, j)
 		j += lp
-		i = Index(test[j:], pattern)
+		i = index(test[j:], pattern, next)
 	}
 	return res
 }
